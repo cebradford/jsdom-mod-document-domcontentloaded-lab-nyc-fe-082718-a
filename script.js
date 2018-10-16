676b75ae85c561b27bfa9995f6c0b83fac104742
casey bradford <casey.bradford@wework.com>
2018-10-16 00:05:19 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ document.addEventListener('DOMContentLoaded', e => {
 document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
 
-document.querySelector("div#hidden-div", 
+document.querySelector("div#hidden-div", e => {innerText + 1})
