21b1241d6c545190ce7667d54b29a0a2423da55f
casey bradford <casey.bradford@wework.com>
2018-10-16 00:01:10 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-      document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+
+addEventListener('DOMContentLoaded', e => {}
+
+document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
